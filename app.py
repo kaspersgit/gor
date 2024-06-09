60d6888e738fa806e909f9a3ae90b918c4073942
Kasper de Harder <kasper.de@king.com>
2024-06-09 23:17:37 +0200

    Added one serious riddle

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, request
+import json
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='staticFiles')
 
 # Sample riddles (replace with your own!)
 riddles = [
-    {"riddle": "I have cities, but no houses. I have mountains, but no trees. I have water, but no fish. What am I?", "answer": "a map"},
-    {"riddle": "What has to be broken before you can use it?", "answer": "an egg"},
+    {"name": "I have it yet I don't", "riddle": "I have cities, but no houses. I have mountains, but no trees. I have water, but no fish. What am I?", "answer": "a map"},
+    {"name": "Odd right?", "riddle": "What has to be broken before you can use it?", "answer": "an egg"},
 ]
+with open('riddles.json') as f:
+    riddles = json.load(f)
 
 current_riddle_index = 0
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def home():
   global current_riddle_index
   if request.method == "POST":
